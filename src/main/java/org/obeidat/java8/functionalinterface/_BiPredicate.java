1e200bd1f798ba68cdcc53c33720f51cdcdb8e15
Mohammad Aktham Obeidat <mohamadaktham7@gmail.com>
2023-07-28 20:26:23 +0300

    Predicate & BiPredicate examples added

---
+++ src/main/java/org/obeidat/java8/functionalinterface/_BiPredicate.java
@@ -0,0 +1,21 @@
+package org.obeidat.java8.functionalinterface;
+
+import java.util.function.BiPredicate;
+
+/**
+ * @author Mohammad Obeidat
+ *
+ * Predicate<T> => Represents a boolean-valued function of two arguments.
+ */
+public class _BiPredicate {
+
+    public static void main(String[] args) {
+
+        System.out.println(isValidJordanianPhoneNumber.test("+962","0747653111"));
+    }
+
+    static BiPredicate<String, String> isValidJordanianPhoneNumber = (countryCode, phoneNumber) -> {
+       return countryCode.equals("+962") && (phoneNumber.startsWith("07") && phoneNumber.length() == 10);
+    };
+
+}
---
+++ src/main/java/org/obeidat/java8/functionalinterface/_Predicate.java
@@ -0,0 +1,23 @@
+package org.obeidat.java8.functionalinterface;
+
+import java.util.function.Predicate;
+
+/**
+ * @author Mohammad Obeidat
+ *
+ * Predicate<T> => Represents a boolean-valued function of one argument.
+ */
+public class _Predicate {
+
+    public static void main(String[] args) {
+        System.out.println(isValidPhoneNumber.test("0747653111"));
+
+        // We can combine more than one Predicate function using [ ".and()" || ".or()" ] methods to make a combined conditions.
+        System.out.println(isValidPhoneNumber.and(containsNumber3).test("0644353111"));
+        System.out.println(isValidPhoneNumber.or(containsNumber3).test("0644353111"));
+    }
+
+    static Predicate<String> isValidPhoneNumber = phoneNumber -> phoneNumber.startsWith("07") && phoneNumber.length() == 10;
+
+    static Predicate<String> containsNumber3 = phoneNumber -> phoneNumber.contains("3");
+}
